bb0cbb3b3fc6fe16dcbeb185ad7cc6045d09990e
Divanio Silva <dcsilva@mail.ru>
2020-05-25 21:33:44 +0100

    Project setup

---
+++ src/main/java/com/ds/kuberneteservice/KuberneteServiceApplication.java
@@ -0,0 +1,13 @@
+package com.ds.kuberneteservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KuberneteServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KuberneteServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ds/kuberneteservice/KuberneteServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ds.kuberneteservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KuberneteServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
